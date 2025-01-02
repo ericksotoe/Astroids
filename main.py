e3da1001bd2cb2ab0610b4867101860e1627ecb4
Erick Soto <ericksotoe@gmail.com>
2025-01-01 21:10:32 -0800

    added pause to keep computer resources low

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         screen.fill(color="black")
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-
+        dt = clock.tick(60.0) / 1000
         pygame.display.flip()
 
     # print("Starting asteroids!")
