0ba8021da22807927e66e81f55d23ca71b434726
Erick Soto <ericksotoe@gmail.com>
2025-01-01 21:34:42 -0800

    added player triangle to screen

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import pygame
-from constants import * 
+from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
-        screen.fill(color="black")
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        dt = clock.tick(60.0) / 1000
+            
+        screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
+        
 
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+        # limit framerate to 60FPS
+        dt = clock.tick(60.0) / 1000
+
+        
+
+        
     
     
 
